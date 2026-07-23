afb5db85b9bc00ebb00fba13d35b7dfd090d1aa4
Guilherme <guimars22@gmail.com>
2026-07-23 15:55:32 -0300

    #

--- lang_chain.py
+++ lang_chain.py
@@ -1,4 +1,4 @@
-
+from langchain.chains.qa_with_sources.stuff_prompt import template
 from langchain_google_genai import ChatGoogleGenecativeAI
 from langchain_community.chat_models import ChatMaritalk
 from langchain_core.messages import HumanMessage
@@ -6,35 +6,49 @@ from langchain_core.messages import HumanMessage
 from my_models import GEMINI_FLASH, MARITACA_SABIA
 from my_keys import GEMINI_API_KEY, MARITACA_API_KEY
 from my_helper import encode_image
+from langchain.prompts import ChatPromptTemplate
+
 
 llm = ChatGoogleGenecativeAI(
     api_key=GEMINI_API_KEY,
     model=GEMINI_FLASH,
 )
-resposta = llm.invoke("Quais canais de Youtuber você recomenda para que eu possa saber mais a respeito de smartphones")
-print("Gemini: ", resposta.content)
+
 
 
 llm = ChatMaritalk(
     api_key=MARITACA_API_KEY,
     model=MARITACA_SABIA,
 )
-resposta = llm.invoke("Quais canais de Youtuber você recomenda para que eu possa saber mais a respeito de smartphones")
-print("Maritaca: ", resposta.content)
+
+template_analisador = ChatPromptTemplate.from_messages(
+    [
+        (
+            "systen",
+            """
+            Assuma que vocé é um analisador de imagens. A sua tarefa principal 
+            consiste em: analisar uma imagem e extrari informações importantes 
+            de forma objetiva.
+            """
+        ),
+        (
+            "user",
+            [
+                {
+                    "type": "text",
+                    "text": "Descreva a imagem: ",
+                },
+                {
+                    "type": "image_url",
+                    "image_url": "data:image/png;base64,{imagem}",
+                }
+            ]
+        )
+    ])
 
 imagem = encode_image("dados\exemplo_grafico.jpg")
-pergunta = "Descreva a imagem: "
 mensagem = HumanMessage(
-    content=[
-        {
-            "type": "text",
-            "text": pergunta,
-        },
-        {
-            "type": "image_url",
-            "image_url": f"data:image/png;base64,{imagem}",
-        }
-    ]
+
 )
 
 resposta = llm.invoke([mensagem])
