7b6835f4171e40ac075868f4b34232523c345cc7
Guilherme <guimars22@gmail.com>
2026-07-24 15:16:18 -0300

    #

--- detalhes_imagem_modelo.py
+++ detalhes_imagem_modelo.py
@@ -1,6 +1,14 @@
-from pydantic import BaseModel
 
+from pydantic import BaseModel, Field
+from typing import List
 
 class DetalhesImagemModelo(BaseModel):
-    descricao: str
-    rotulos: list[str]
+    titulo: str = Field(
+        descriptor="Defina o titulo adequando para a imagem que foi analisada."
+    )
+    descricao: str = Field(
+        descriptor="Coloque aqui uma descrição detalhada de sua análise para imagem."
+    )
+    rotulos: List[str] = Field(
+        description="Defina três rótulos principais para a imagem analisada."
+    )
