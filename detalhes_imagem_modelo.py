3e7ecda30fb59d55dca417f6394370ced99b399e
Guilherme <guimars22@gmail.com>
2026-07-24 15:18:03 -0300

    mend

--- detalhes_imagem_modelo.py
+++ detalhes_imagem_modelo.py
@@ -4,10 +4,10 @@ from typing import List
 
 class DetalhesImagemModelo(BaseModel):
     titulo: str = Field(
-        descriptor="Defina o titulo adequando para a imagem que foi analisada."
+        description="Defina o titulo adequando para a imagem que foi analisada."
     )
     descricao: str = Field(
-        descriptor="Coloque aqui uma descrição detalhada de sua análise para imagem."
+        description="Coloque aqui uma descrição detalhada de sua análise para imagem."
     )
     rotulos: List[str] = Field(
         description="Defina três rótulos principais para a imagem analisada."
