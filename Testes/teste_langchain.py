d8e2d50e388bfb8e9ac2989252eb7394e5a894ac
Guilherme <guimars22@gmail.com>
2026-07-24 14:51:26 -0300

    #

--- Testes/teste_langchain.py
+++ Testes/teste_langchain.py
@@ -1,6 +1,13 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
+from my_keys import GEMINI_API_KEY
 
 llm = ChatGoogleGenerativeAI(
     model="gemini-3.6-flash",
     google_api_key=GEMINI_API_KEY
 )
+
+resposta = llm.invoke(
+    "Explique o que é LangChain em uma frase."
+)
+
+print(resposta.content)
