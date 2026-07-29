4b508a102c85d06c6fb441265ce30e39577e77ec
Guilherme <guimars22@gmail.com>
2026-07-29 17:43:02 -0300

    #

--- main.py
+++ main.py
@@ -1,17 +1,52 @@
 from langchain.agents import AgentExecutor
 from agente.orquestrador import AgenteOrquestrador
 
+
 def main():
     agente = AgenteOrquestrador()
+
     orquestrador = AgentExecutor(
         agent=agente.agente,
         tools=agente.tools,
         verbose=True,
     )
 
-    pergunta = "Gostaria que você me explicasse como funcionam os devios condicionais"
-    resposta = orquestrador.invoke({"input": pergunta})
-    print(resposta)
+    print("""
+    === Assistente IA ===
+
+    Exemplos:
+    - Explique o que são desvios condicionais
+    - Faça uma análise da imagem exemplo_grafico.jpg
+
+    Digite 'sair' para encerrar.
+    """)
+
+    while True:
+        pergunta = input("Você: ")
+
+        if pergunta.lower() in ["sair", "exit", "quit"]:
+            print("Até logo!")
+            break
+
+        try:
+            resposta = orquestrador.invoke({
+                "input": pergunta
+            })
+
+            print("""
+                === Assistente IA ===
+
+                Exemplos:
+                - Explique o que são desvios condicionais
+                - Faça uma análise da imagem exemplo_grafico.jpg
+
+                Digite 'sair' para encerrar.
+            """)
+            print(resposta["output"])
+            print()
+
+        except Exception as e:
+            print(f"\nErro: {e}\n")
 
 
 if __name__ == "__main__":
